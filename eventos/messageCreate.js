87968fd6284fb5ebcbdc0f63452a5a956c08bf43
Pedro Larrosa Caselles <larrosacasellespedro@gmail.com>
2021-10-28 20:07:31 +0200

    case sensitive

--- eventos/messageCreate.js
+++ eventos/messageCreate.js
@@ -2,9 +2,45 @@ const fs = require("fs");
 const prefix = require("..");
 const geolib = require("geolib");
 
-let coordenadas = JSON.parse(
+const coordenadas = JSON.parse(
     fs.readFileSync(__dirname + "/../localidades.json", "utf-8")
 );
+
+const normalizar = (s) => {
+    s = s.toLowerCase();
+
+    if (!s.includes(" ")) {
+        return s.replace(s[0], s[0].toUpperCase());
+    }
+
+    let splited = s.split(" ").map((word) => {
+        if (word !== "de") {
+            return word.replace(word[0], word[0].toUpperCase());
+        }
+
+        return word;
+    });
+
+    return splited.reduce((prev, word) => (prev += " " + word));
+};
+
+const getDistanceBetween = (p1, p2) => {
+    return geolib
+        .convertDistance(
+            geolib.getDistance(
+                {
+                    latitude: coordenadas[p1].latitud,
+                    longitude: coordenadas[p1].longitud
+                },
+                {
+                    latitude: coordenadas[p2].latitud,
+                    longitude: coordenadas[p2].longitud
+                }
+            ),
+            "km"
+        )
+        .toFixed(2);
+};
 
 module.exports = {
     name: "messageCreate",
@@ -27,25 +63,17 @@ module.exports = {
                 return;
             }
 
+            parametros[0] = normalizar(parametros[0]);
+            parametros[1] = normalizar(parametros[1]);
+
             if (
                 Object.keys(coordenadas).includes(parametros[0]) &&
                 Object.keys(coordenadas).includes(parametros[1])
             ) {
-                let distancia = geolib
-                    .convertDistance(
-                        geolib.getDistance(
-                            {
-                                latitude: coordenadas[parametros[0]].latitud,
-                                longitude: coordenadas[parametros[0]].longitud
-                            },
-                            {
-                                latitude: coordenadas[parametros[1]].latitud,
-                                longitude: coordenadas[parametros[1]].longitud
-                            }
-                        ),
-                        "km"
-                    )
-                    .toFixed(2);
+                let distancia = getDistanceBetween(
+                    parametros[0],
+                    parametros[1]
+                );
 
                 message.channel.send(
                     `La distancia entre ${parametros[0]} y ${parametros[1]} es de ${distancia}km`
